043ac103660d16f05403582256baadce398c8c3d
poorvika1234578890 <151720694+poorvika1234578890@users.noreply.github.com>
2025-02-02 13:14:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const questions = [
+    {
+        question:"Which is the largest animal in the world?",
+        answers:[
+            {text:"shark", correct:false},
+            {text:"blue whale", correct:true},
+            {text:"elephent" ,correct:false},
+            {text:"Monkey" ,correct:false},
+
+            
+ ]
+    }
+];
+const question = document.getElementById("question");
+const answerbuttons = document.getElementById("answer-buttons");
+const nextbutton = document.getElementById("next");
